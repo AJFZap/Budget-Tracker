c4dcc9ad8759f5c4b2ed3a4fd25ea4421ea736c7
AJFZap <ariel_zappia@hotmail.com>
2024-06-06 13:19:59 -0300

    Password Reset now working properly with link expiration

--- authentication/urls.py
+++ authentication/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [path('register', RegistrationView.as_view(), name="register"),
 
                path('reset_password', auth_views.PasswordResetView.as_view(template_name="authentication/password_reset.html"), name='reset_password'),
                path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="authentication/password_reset_done.html"), name='password_reset_done'),
-               path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="authentication/password_reset_confirm.html"), name='password_reset_confirm'),
-               path('reset_password_success/', auth_views.PasswordResetCompleteView.as_view(template_name="authentication/password_reset_complete.html"), name='password_reset_complete'),
+               path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='authentication/password_reset_confirm.html'), name='password_reset_confirm'),
+               path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='authentication/password_reset_complete.html'), name='password_reset_complete'),
                ]
