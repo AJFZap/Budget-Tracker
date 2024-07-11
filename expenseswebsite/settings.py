2aa6fc804785ebf87c10458ed60f4e5ef028dd11
AJFZap <ariel_zappia@hotmail.com>
2024-07-11 12:15:37 -0300

    Vercel deployment

--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -28,9 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 # postgresql://postgres:vqtdkFtphCgqBtdBfpUeSROuFrHAwcPR@viaduct.proxy.rlwy.net:29638/railway
-ALLOWED_HOSTS = ['127.0.0.1', 'bg-tracker.up.railway.app']
-
-CSRF_TRUSTED_ORIGINS =['https://bg-tracker.up.railway.app/']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 # Application definition
 
