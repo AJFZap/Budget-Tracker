a612933a25f48ebd8c61505b281d7e33aef25358
AJFZap <ariel_zappia@hotmail.com>
2024-07-18 12:22:00 -0300

    Added Regex to match wrong URL nationalization on Non-Auth

--- static/js/main.js
+++ static/js/main.js
@@ -15,14 +15,18 @@ document.addEventListener('DOMContentLoaded', function() {
         const preferences = JSON.parse(localStorage.getItem('preferences'));
         // Get Language.
         const prefLanguage = preferences.language;
-        console.log(prefLanguage);
+        // console.log(prefLanguage);
 
         // Get currentUrl and check if it's the same language as the preferred one
         const currentUrl = window.location.href;
-        console.log(currentUrl);
+        // console.log(currentUrl);
         
-        const newUrl = currentUrl.replace('/en/' || '/es/' || '/ja/', '/' + prefLanguage + '/');
-        console.log(newUrl);
+        // Define a regular expression to match the language segment in the URL.
+        const langRegex = /\/(en|es|ja)\//;
+        
+        // Replace the URL with the one we want.
+        const newUrl = currentUrl.replace(langRegex, '/' + prefLanguage + '/');
+        // console.log(newUrl);
 
         // If the language it's different to the saved one then we change the url.
         if (currentUrl != newUrl) {
