f2d8d3b13ada588565d77262ffbe9dbc14b347b5
AJFZap <ariel_zappia@hotmail.com>
2024-07-20 14:52:20 -0300

    Update TRUSTES_ORIGINS

--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'bg-tracker.up.railway.app/', '*']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://bg-tracker.up.railway.app/',
+    'https://*.railway.app'
 ]
 
 CSRF_COOKIE_HTTPONLY = False
